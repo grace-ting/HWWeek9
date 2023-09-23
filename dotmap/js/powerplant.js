df69aa406e27107ee7d19033350fcb9b6aaa958e
grace-ting <72272253+grace-ting@users.noreply.github.com>
2023-09-23 16:48:17 +0800

    Update powerplant.js

--- dotmap/js/powerplant.js
+++ dotmap/js/powerplant.js
@@ -1,4 +1,4 @@
-var vg_1 = "./js/powerplant.vg.json";
+var vg_1 = "./js/dotmap.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
